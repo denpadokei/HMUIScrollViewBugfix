2f7fcecb50bcdb55792d45cd6c1f228a5d53b96b
デンパ時計 <jsoldenpa@yahoo.co.jp>
2022-10-13 01:05:03 +0900

    add scrollPageSize

--- HMUIScrollViewBugfix/Pathces/ScrollViewPatch.cs
+++ HMUIScrollViewBugfix/Pathces/ScrollViewPatch.cs
@@ -5,12 +5,32 @@ using UnityEngine;
 namespace HMUIScrollViewBugfix.Pathces
 {
     [HarmonyPatch(typeof(ScrollView), "contentSize", MethodType.Getter)]
-    internal class ScrollViewPatch
+    internal class ScrollViewget_contentSizePatch
     {
         public static bool Prefix(RectTransform ____contentRectTransform, ref float __result)
         {
             try {
                 if (____contentRectTransform == null || ____contentRectTransform.rect == null) {
+                    __result = 0f;
+                    return false;
+                }
+            }
+            catch (System.Exception e) {
+                Plugin.Log.Error(e);
+                __result = 0f;
+                return false;
+            }
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(ScrollView), "scrollPageSize", MethodType.Getter)]
+    internal class ScrollViewget_scrollPageSizePatch
+    {
+        public static bool Prefix(RectTransform ____viewport, ref float __result)
+        {
+            try {
+                if (____viewport == null || ____viewport.rect == null) {
                     __result = 0f;
                     return false;
                 }
