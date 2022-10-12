4a29cf411005e8838ad604f968c0c22f85ca7aaf
デンパ時計 <jsoldenpa@yahoo.co.jp>
2022-10-12 19:10:15 +0900

    プロジェクト ファイルを追加します。

---
+++ HMUIScrollViewBugfix/Pathces/ScrollViewget_contentSizePatch.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using HMUI;
+using UnityEngine;
+
+namespace HMUIScrollViewBugfix.Pathces
+{
+    [HarmonyPatch(typeof(ScrollView), "contentSize", MethodType.Getter)]
+    internal class ScrollViewget_contentSizePatch
+    {
+        public static bool Prefix(RectTransform ____contentRectTransform, ref float __result)
+        {
+            try {
+                if (____contentRectTransform == null || ____contentRectTransform.rect == null) {
+                    __result = 0f;
+                    return false;
+                }
+            }
+            catch (System.Exception e) {
+                Plugin.Log.Error(e);
+                __result = 0f;
+                return false;
+            }
+            return true;
+        }
+    }
+}
